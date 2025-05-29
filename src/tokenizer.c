0d3ef0282075654123fcc6b4678e133080c994f8
Stephen Adesina <bauerstevenson@gmail.com>
2025-05-30 01:24:40 +0300

    adding invalid chars support

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,32 +1,42 @@
 #include "tokenizer.h"
 #include <stdio.h>
+#include <string.h>
 
 char *tokens[100];
 int size = 0;
 
 void tokenize(char *file_contents) {
-    for (int i =0; file_contents[i] != '\0'; i++) {
-        char token = file_contents[i];
-        switch (token) {
-            case '(':  addToken("LEFT_PAREN ("); break;
-            case ')': addToken("RIGHT_PAREN )"); break;
-            case '{':  addToken("LEFT_BRACE {"); break;
-            case '}': addToken("RIGHT_BRACE }"); break;
-            case ',':  addToken("COMMA ,"); break;
-            case '.': addToken("DOT ."); break;
-            case '-':  addToken("MINUS -"); break;
-            case '+': addToken("PLUS +"); break;
-            case ';': addToken("SEMICOLON ;"); break;
-            case '/':  addToken("SLASH /"); break;
-            case '*': addToken("STAR *"); break;
+    char *line = strtok(file_contents,"\n");
+    int line_count = 1;
+    char *error[256];
+//can rewrite this to have a token data structure and not add error to tokens
+    while(line) {
+        for (int i =0; line[i] != '\0'; i++) {
+            char token = line[i];
+            switch (token) {
+                case '(':  addToken("LEFT_PAREN ( null"); break;
+                case ')': addToken("RIGHT_PAREN ) null"); break;
+                case '{':  addToken("LEFT_BRACE { null"); break;
+                case '}': addToken("RIGHT_BRACE } null"); break;
+                case ',':  addToken("COMMA , null"); break;
+                case '.': addToken("DOT . null"); break;
+                case '-':  addToken("MINUS - null"); break;
+                case '+': addToken("PLUS + null"); break;
+                case ';': addToken("SEMICOLON ; null"); break;
+                case '/':  addToken("SLASH / null"); break;
+                case '*': addToken("STAR * null"); break;
+                default: snprintf(error, sizeof(error),"[line %d] Error: Unexpected character: %c", line_count, token);addToken(error);
+            }
         }
+        line_count +=1;
+        line = strtok(NULL,"\n");
     }
 }
 
 void printTokens() {
     for(int i=0; i<size; i++) {
         setvbuf (stdout, NULL, _IONBF, 0);
-        printf("%s %s\n",tokens[i], "null");
+        printf("%s\n",tokens[i]);
     }
     printf("EOF  null");
 
